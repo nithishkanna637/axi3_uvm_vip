6b833d164c61a7a16321475f72ace5d2a9896698
Nithish kannan s <nithishkanna637@gmail.com>
2026-05-19 11:52:10 +0530

    Update axi_common.sv

--- tb/axi_common.sv
+++ tb/axi_common.sv
@@ -1,8 +1,8 @@
 `define addr_width 32
 `define data_width 32
 `define strb_width `data_width/8
-`define N0_OF_TX 10
- int drive_count;
+`define NO_OF_TX 20
+  int drive_count;
 `define NEW_COMP \
  function new(input string name ="",uvm_component parent); \
  	 super.new(name,parent); \
