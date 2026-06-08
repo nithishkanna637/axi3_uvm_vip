1536791d962739f375f958414b9c157f092d8581
Nithish kannan s <nithishkanna637@gmail.com>
2026-06-08 18:40:35 +0530

    Update axi_env.sv

--- tb/axi_env.sv
+++ tb/axi_env.sv
@@ -1,11 +1,19 @@
 class axi_env extends uvm_env;
 axi_magent magent;
 axi_sagent sagent;
+axi_scoreboard sbd;
 `uvm_component_utils(axi_env)
 `NEW_COMP
 function void build_phase(uvm_phase phase);
   super.build_phase(phase);
   magent=axi_magent::type_id::create("magent",this);
   sagent=axi_sagent::type_id::create("sagent",this);
+  sbd=axi_scoreboard::type_id::create("sbd",this);
 endfunction
+
+function void connect_phase(uvm_phase phase);
+  super.connect_phase(phase);
+  magent.mon.ap_port.connect(sbd.sb_imp);
+endfunction
+
 endclass
